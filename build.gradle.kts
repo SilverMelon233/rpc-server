de1a9f0dbc2db211298a22b9274cad2e70235195
yutingrui <yutingrui@tal>
2026-04-25 10:52:36 +0800

    chore(kotlin): grpc 1.80.0, kotlin 2.3.21, coroutines 1.10.2, shadow 9.4.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,20 @@
 plugins {
-    kotlin("jvm") version "2.0.21"
+    kotlin("jvm") version "2.3.21"
     application
-    id("com.gradleup.shadow") version "9.0.0-beta9"
+    id("com.gradleup.shadow") version "9.4.1"
 }
-group = "com.silvermelos233"
-repositories {
-    mavenCentral()
-    }
+group = "com.example"
+repositories { mavenCentral() }
 dependencies {
-    implementation("io.grpc:grpc-netty-shaded:1.73.0")
-    implementation("io.grpc:grpc-protobuf:1.73.0")
+    implementation("io.grpc:grpc-netty-shaded:1.80.0")
+    implementation("io.grpc:grpc-stub:1.80.0")
+    implementation("io.grpc:grpc-protobuf:1.80.0")
     implementation("io.grpc:grpc-kotlin-stub:1.5.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
+    implementation("com.google.protobuf:protobuf-java:4.34.1")
+    implementation("com.google.protobuf:protobuf-kotlin:4.34.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
     compileOnly("org.apache.tomcat:annotations-api:6.0.53")
 }
 application {
     mainClass.set("ServerKt")
-}
-tasks.jar {
-    manifest {
-        attributes["Main-Class"] = "ServerKt"
-    }
 }
